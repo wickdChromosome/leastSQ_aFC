c8cdaad799537933d8ca347dcc343ffebebdea14
Bence Kotis <bkotis@pejlab.scripps.edu>
2019-07-25 14:39:03 -0700

    still cleaning

--- sort_file.py
+++
@@ -1,8 +0,0 @@
-import sys
-import pandas as pd
-
-pd.read_csv(sys.argv[1],sep='\t',header=None).sort_values(by=[1]).to_csv(sys.argv[2], header=None, index=None, sep='\t')
-#print(pd.read_csv(sys.argv[1],sep='\t', header=None).columns)
-
-
-
